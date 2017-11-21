875a25813bb466ba40914b944cc9be1935f8a12f
beaubbe <gabriel.couvrette@gmail.com>
2017-11-20 21:26:26 -0500

    Added unit test project.

---
+++ typeCalc/UnitTests/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{		
+	TEST_CLASS(TestClassName)
+	{
+	public:		
+		TEST_METHOD(TestMethodName)
+		{
+			// TODO: Your test code here
+			Assert::IsTrue(false);
+		}
+
+	};
+}
